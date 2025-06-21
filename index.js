cd1bf149fbe00d1e0c31bdad3e7be50c5f250012
horaceo1b <horaceken0@gmail.com>
2025-06-21 17:59:19 +0300

    first commit

--- index.js
+++ index.js
@@ -46,3 +46,32 @@ const nestedMuppet = {
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+
+
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+
+
+ const [r, o, y, g, b, , v] = colors;
+
+
+const { muppetName, color, song, job, partner } = muppet;
+
+const {
+  album: {
+    theMuppetMovie: { song2, song4 }
+  },
+  nestedJob,
+  nestedPartner
+} = nestedMuppet;
+
+
+const [bessie, star, dolly, babe, little] = farmAnimals.split(' ');
+
+
+const [blackAndWhite, black, pink] = ['cow', 'sheep', 'pig'];
+
+
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+
+ 
+const [, , , , , indg] = colors;
